d178eb3a392715d041b87c66584d3465cfb9eed1
Vika <vika@MacBook-Pro-Vika.local>
2024-06-27 13:36:22 +0700

    style: add jumbotron in content page

--- src/app/[locale]/(unauth)/content/page.tsx
+++ src/app/[locale]/(unauth)/content/page.tsx
@@ -1,6 +1,8 @@
 import { getTranslations } from 'next-intl/server';
 
-import { Navbar } from '@/templates/Navbar';
+import { Badge } from '@/components/ui/badge';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 export async function generateMetadata(props: { params: { locale: string } }) {
   const t = await getTranslations({
@@ -17,14 +19,27 @@ export async function generateMetadata(props: { params: { locale: string } }) {
 export default function IndexPage() {
   return (
     <>
-      <Navbar />
-      {/* <Hero />
-      <Sponsors />
-      <Features />
-      <Pricing />
-      <FAQ />
-      <CTA />
-      <Footer /> */}
+      <div className="flex h-80 w-full flex-col items-center justify-center bg-black py-8">
+        <h1 className="text-[50px] font-bold text-white">
+          Discover the best Content
+        </h1>
+        <h5 className="my-2 text-[#7d7d7d]">
+          Explore a curated collection of the latest and greatest articles,
+          tutorials, and more.
+        </h5>
+        <div className="flex w-[33%] items-center justify-center">
+          <Input
+            placeholder="Enter url"
+            className="border-none bg-white outline-none"
+          />
+          <Button className="ml-2 bg-transparent text-white">Submit</Button>
+        </div>
+      </div>
+      <div className="flex w-full flex-col p-8">
+        <div className="flex w-full gap-2">
+          <Badge variant="outline">Badge</Badge>
+        </div>
+      </div>
     </>
   );
 }
